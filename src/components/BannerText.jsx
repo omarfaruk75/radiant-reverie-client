aeb719a0acdc4c843d64c44ec5e41e96a06cdc89
omar faruk <farukomarcpa@gmail.com>
2024-05-14 14:00:52 +0600

    text updated

--- src/components/BannerText.jsx
+++ src/components/BannerText.jsx
@@ -8,7 +8,7 @@ const BannerText = () => {
             <h1 className="text-5xl text-[#6E6B58] font-bold animate__animated animate__pulse animate_delay_2s">Radiant Reverie Salon!</h1>
             <p className="py-6 px-8 text-[#222222]">
 
-                Radiant Reverie Salon offers a serene sanctuary for beauty and relaxation. With meticulous attention to detail and a skilled team, it provides personalized services tailored to each client's needs. From luxurious hair treatments to rejuvenating spa experiences, it's a haven where guests emerge feeling refreshed and radiant.</p>
+                Radiant Reverie Salon offers a serene sanctuary for beauty and relaxation. With meticulous attention to detail and a skilled team, it provides personalized services tailored to each clients needs. From luxurious hair treatments to rejuvenating spa experiences, it is a haven where guests emerge feeling refreshed and radiant.</p>
             <button className="btn bg-[#6E6B58] text-white">Get Started</button>
 
 
